b3a1374734f049ded814a1e8ce84e4f2f51ec982
Lucas Tamura <lucas01.santos@grupojacto.com.br>
2024-05-06 21:06:47 -0300

    first commit

---
+++ insert.sql
@@ -0,0 +1,94 @@
+// Inserções para a tabela PedidosPorCliente
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Burk', 'Apt 970', 'bwheatcroft0@theguardian.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Aloysia', 'Room 1241', 'ababin1@epa.gov');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Marybeth', 'Apt 1526', 'mborleace2@1und1.de');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Vincenz', 'Suite 59', 'vpahler3@ihg.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Herminia', 'Apt 1489', 'hhastelow4@gov.uk');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Katherina', '5th Floor', 'kdenning5@google.co.uk');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Gloria', '17th Floor', 'gsearby6@wufoo.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Elinor', 'Suite 74', 'ejakobsson7@weather.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Consalve', 'Suite 7', 'cbrearty8@scribd.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Suzann', 'Room 358', 'swickerson9@google.de');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Marnie', 'PO Box 74012', 'mkeevesa@msu.edu');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Christiana', 'Room 25', 'csaxonb@howstuffworks.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Lianne', 'PO Box 75566', 'lsteedc@people.com.cn');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Pru', 'Apt 1067', 'pdevoyd@paginegialle.it');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Fidelity', '2nd Floor', 'ftroope@google.co.jp');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Philly', '16th Floor', 'pwanjekf@blogs.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Sunny', 'Apt 443', 'shelderg@tamu.edu');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Yasmin', 'Room 1510', 'ywolfersh@slate.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Amory', 'Room 328', 'asherstoni@rediff.com');
+insert into PedidosPorCliente (ClienteID, PedidoID, DataPedido, Nome, Endereco, Email) values (uuid(), uuid(), toTimestamp(now()),'Doralynn', 'PO Box 6065', 'dguarej@bloglines.com');
+
+
+// Inserções para a tabela ProdutosPorCategoria
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Pineapple - Golden', 97.99);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Wine - Barbera Alba Doc 2001', 944.64);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Chicken - Breast, 5 - 7 Oz', 883.34);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Paper - Brown Paper Mini Cups', 251.6);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Bread - Bistro White', 600.96);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Stainless Steel Cleaner Vision', 213.05);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Veal - Eye Of Round', 451.83);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Flour - Strong', 104.95);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Plaintain', 263.23);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Ice Cream - Super Sandwich', 583.75);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Langers - Mango Nectar', 212.59);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Petit Baguette', 794.82);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Pasta - Bauletti, Chicken White', 551.12);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Wine - Pinot Noir Latour', 477.12);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Turnip - Mini', 954.32);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Petit Baguette', 324.93);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'French Pastries', 666.77);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Mix - Cocktail Strawberry Daiquiri', 237.68);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Gingerale - Schweppes, 355 Ml', 282.64);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values (uuid(), uuid(),'Pasta - Canelloni', 26.94);
+insert into ProdutosPorCategoria (CategoriaID, ProdutoID, Nome, Preco) values ('417ac966-1a57-4ce8-b70f-716db550995d', uuid(),'Pasta - Talharin', 38.94);
+
+
+
+// Inserções para a tabela ItensPorPedido
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),10);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),29);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),9);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),10);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),30);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),21);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),33);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),50);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),13);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),32);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),45);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),35);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),12);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),25);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),8);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),4);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),15);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),13);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),14);
+insert into ItensPorPedido (PedidoID, ProdutoID, Quantidade) values (uuid(), uuid(),13);
+
+
+// Inserções para a tabela VendasPorProduto
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 1 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 3 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 65 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 58 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 65 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 25 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 5 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 6 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 7 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 8 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 9 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 1 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 23 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 63 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 73 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 34 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 37 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 82 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 123 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 71 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 73 WHERE ProdutoID = uuid();
+UPDATE VendasPorProduto SET QuantidadeVendida = QuantidadeVendida + 73 WHERE ProdutoID = uuid();
